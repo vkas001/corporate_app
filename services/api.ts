f04d830a5f551603ab71f7cda314c05aa9ddc879
vkas001 <bikash.795424@cct.tu.edu.np>
2026-01-05 18:27:01 +0545

    api modified

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import { clearAuth, getAuth } from "@/utils/auth";
 import axios from "axios";
 import { router } from "expo-router";
 
-const API_BASE_URL = "process.env.EXPO_PUBLIC_API_BASE_URL!";
+const API_BASE_URL = "process.env.EXPO_PUBLIC_API_BASE_URL";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
